6cfeb150d320f0c42d82e843fe78840bbb8512ab
Katy Solovewicz <katy@solovewicz.com>
2020-03-24 11:36:15 -0700

    add code to Views/Author/Add & Create
    Co-authored-by: Jeremy Kale Padot <jeremy@padot.us>
    Co-authored-by: Steph Podolak <StephPodolak@gmail.com>

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-@{
-  Layout = "_Layout";
-}
+using Microsoft.AspNetCore.Mvc;
 
-@Html.ActionLink("View Books", "Index", "Books", null, new { @class = "mdl-button mdl-js-button mdl-button--raised" })
-@Html.ActionLink("View Authors", "Index", "Authors",  null, new { @class = "mdl-button mdl-js-button mdl-button--raised" })
+namespace Library.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
